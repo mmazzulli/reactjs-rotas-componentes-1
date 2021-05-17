a09f463da193400ec01d1e5b779dd6adf63090b0
webmarchesin <webmarchesin@gmail.com>
2021-05-17 23:48:08 +0100

    First Upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Routes from './components/Routes'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+        <Header/>
+        <Routes/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home(){
+    return (
+        <div className="container">
+        <h1>Página Home</h1>
+        </div>
+    )
+}
+
+export default Home;
